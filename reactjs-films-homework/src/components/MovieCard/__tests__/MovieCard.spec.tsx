1b7b212c3a9e1d301b9a617f0cf959d7871e5743
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2021-06-24 19:02:26 +0300

    feat; added tests for ActorProfile component

--- reactjs-films-homework/src/components/MovieCard/__tests__/MovieCard.spec.tsx
+++ reactjs-films-homework/src/components/MovieCard/__tests__/MovieCard.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent, cleanup } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import MovieCard from '../MovieCard';
 
 describe('MovieCard', () => {
