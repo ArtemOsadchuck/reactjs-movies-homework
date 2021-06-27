ed8bac6442be062f03fe43e571c0984069ae981b
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2021-06-27 12:18:54 +0300

    Revert "feat: added translation functionality"
    This reverts commit ab1a574ace82bb71bb73b4fd3a683b21eacb4726.

--- reactjs-films-homework/src/pages/Main/CategoriesTabs/CategoriesTabs.tsx
+++ reactjs-films-homework/src/pages/Main/CategoriesTabs/CategoriesTabs.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import styles from './CategoriesTabs.module.scss';
 
-import lang from '../../../languages/getLanguage';
-import { useAppSelector } from '../../../hooks/hooks';
-
 const CategoriesTabs: React.FC = () => {
-  const appLang = useAppSelector((state) => state.languageReducer.lang);
-
   return (
     <div className={styles.categoriesWrapper}>
-      <div>{lang(appLang).popular}</div>
-      <div>{lang(appLang).topRated}</div>
-      <div>{lang(appLang).upcoming}</div>
+      <div>Popular</div>
+      <div>Top rated</div>
+      <div>Upcoming</div>
     </div>
   );
 };
--- reactjs-films-homework/src/pages/Main/CategoriesTabs/__tests__/CategoriesTabs.spec.tsx
+++ reactjs-films-homework/src/pages/Main/CategoriesTabs/__tests__/CategoriesTabs.spec.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import { cleanup, render } from '@testing-library/react';
 import CategoriesTabs from '../CategoriesTabs';
-import { Provider } from 'react-redux';
-import store from '../../../../store/store';
+
 describe('CategoriesTabs', () => {
-  cleanup();
+  afterEach(() => {
+    cleanup();
+  });
 
   it('CategoriesTabs snapshot', () => {
-    const asFragment = render(
-      <Provider store={store}>
-        <CategoriesTabs />
-      </Provider>
-    );
+    const { asFragment } = render(<CategoriesTabs />);
     expect(asFragment).toMatchSnapshot();
-    cleanup();
   });
 });
