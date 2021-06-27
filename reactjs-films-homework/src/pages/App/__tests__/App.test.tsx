22f3932629bff6f924fbbc0f493e481e5120619d
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2021-06-27 11:08:11 +0300

    fix: App test to redux

--- reactjs-films-homework/src/pages/App/__tests__/App.test.tsx
+++ reactjs-films-homework/src/pages/App/__tests__/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import { Provider } from 'react-redux';
+import store from '../../../store/store';
 
 test('App test', () => {
-  const { asFragment } = render(<App />);
-  //   screen.debug();
+  const asFragment = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  ); //   screen.debug();
   expect(asFragment).toMatchSnapshot();
 });
