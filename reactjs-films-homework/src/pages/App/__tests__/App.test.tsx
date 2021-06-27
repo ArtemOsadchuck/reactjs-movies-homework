7bdd1d15afb9a48141d1620dba4e3b5a0fffcc6b
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2021-06-27 12:17:06 +0300

    Revert "fix: App test to redux"
    This reverts commit 22f3932629bff6f924fbbc0f493e481e5120619d.

--- reactjs-films-homework/src/pages/App/__tests__/App.test.tsx
+++ reactjs-films-homework/src/pages/App/__tests__/App.test.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
-import { Provider } from 'react-redux';
-import store from '../../../store/store';
 
 test('App test', () => {
-  const asFragment = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  ); //   screen.debug();
+  const { asFragment } = render(<App />);
+  //   screen.debug();
   expect(asFragment).toMatchSnapshot();
 });
