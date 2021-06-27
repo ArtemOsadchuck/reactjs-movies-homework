38a95b6ad624d0fc896e8738499729ebdb25aaf5
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2021-06-27 12:20:01 +0300

    Revert "feat: added store"
    This reverts commit 473dc049780e7e76f0af47776fe0083036caff1a.

--- reactjs-films-homework/src/index.tsx
+++ reactjs-films-homework/src/index.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './pages/App/App';
-import { Provider } from 'react-redux';
-import store from './store/store';
+import App from './pages/App';
 
-// console.log(Provider);
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
