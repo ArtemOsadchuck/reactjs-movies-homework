cbeb66524d61da0aded1ebe809d823efe9645540
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2021-06-29 17:38:10 +0300

    feat: added fetchMainData to the CategoriesTabs component

--- reactjs-films-homework/src/pages/Main/CategoriesTabs/CategoriesTabs.tsx
+++ reactjs-films-homework/src/pages/Main/CategoriesTabs/CategoriesTabs.tsx
@@ -2,16 +2,41 @@ import React from 'react';
 import styles from './CategoriesTabs.module.scss';
 
 import lang from '../../../languages/getLanguage';
-import { useAppSelector } from '../../../hooks/hooks';
+import { useAppSelector, useAppDispatch } from '../../../hooks/hooks';
+import { setCategory } from '../../../store/rootStore/langStore/languageSlice';
+
+import fetchMainData from '../../../store/rootStore/langStore/fetchMainData';
 
 const CategoriesTabs: React.FC = () => {
   const appLang = useAppSelector((state) => state.languageReducer.lang);
+  const dispatch = useAppDispatch();
+
+  const mainState = {
+    lang: useAppSelector((state) => state.languageReducer.lang),
+    page: useAppSelector((state) => state.languageReducer.page),
+  };
+
+  const getPopular = async () => {
+    const mainStateAddCategory = { ...mainState, category: 'popular' };
+    await dispatch(setCategory('popular'));
+    await dispatch(fetchMainData(mainStateAddCategory));
+  };
+  const getTopRated = async () => {
+    const mainStateAddCategory = { ...mainState, category: 'top_rated' };
+    await dispatch(setCategory('top_rated'));
+    await dispatch(fetchMainData(mainStateAddCategory));
+  };
+  const getUpcoming = async () => {
+    const mainStateAddCategory = { ...mainState, category: 'upcoming' };
+    await dispatch(setCategory('upcoming'));
+    await dispatch(fetchMainData(mainStateAddCategory));
+  };
 
   return (
     <div className={styles.categoriesWrapper}>
-      <div>{lang(appLang).popular}</div>
-      <div>{lang(appLang).topRated}</div>
-      <div>{lang(appLang).upcoming}</div>
+      <div onClick={getPopular}>{lang(appLang).popular}</div>
+      <div onClick={getTopRated}>{lang(appLang).topRated}</div>
+      <div onClick={getUpcoming}>{lang(appLang).upcoming}</div>
     </div>
   );
 };
