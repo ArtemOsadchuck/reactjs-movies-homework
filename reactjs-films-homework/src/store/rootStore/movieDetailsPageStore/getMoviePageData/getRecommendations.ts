c681db17f03bd85b3652afe45e6799a1d187e093
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2021-07-06 22:51:12 +0300

    feat: added simple try-catch construction

--- reactjs-films-homework/src/store/rootStore/movieDetailsPageStore/getMoviePageData/getRecommendations.ts
+++ reactjs-films-homework/src/store/rootStore/movieDetailsPageStore/getMoviePageData/getRecommendations.ts
@@ -9,10 +9,9 @@ export interface IGetRecommendations {
 const getRecommendations = createAsyncThunk(
   'getRecommendations/setFetchRecommendations',
   async (props: IGetRecommendations) => {
-    const { lang, movie_id } = props;
-
-    const baseUrl = `https://api.themoviedb.org/3/movie/${movie_id}/recommendations?api_key=8fa5bc53bb4a09dfb6560253edf33030&language=${lang.toLowerCase()}-${lang}&page=1`;
     try {
+      const { lang, movie_id } = props;
+      const baseUrl = `https://api.themoviedb.org/3/movie/${movie_id}/recommendations?api_key=8fa5bc53bb4a09dfb6560253edf33030&language=${lang.toLowerCase()}-${lang}&page=1`;
       const response = await axios.get(baseUrl);
       return response.data;
     } catch (error) {
