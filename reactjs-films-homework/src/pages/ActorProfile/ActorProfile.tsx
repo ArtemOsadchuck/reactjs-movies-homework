31ed5ec330cc2b614ba08f58a273b8a39bfbba32
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2021-07-06 11:56:34 +0300

    feat: added store to ActorPrfile.tsx
    implemented work with store

--- reactjs-films-homework/src/pages/ActorProfile/ActorProfile.tsx
+++ reactjs-films-homework/src/pages/ActorProfile/ActorProfile.tsx
@@ -1,7 +1,4 @@
-import React, { useState, useMemo, useEffect } from 'react';
-import actorInfo from '../../mocks/actorInfo';
-import actorPhotosMock from '../../mocks/actorPhotos';
-import filmsWithActor from '../../mocks/filmsWithActor';
+import React, { useState, useEffect } from 'react';
 
 import ActorTitle from './ActorTitleInfo';
 import ActorPhotos from './ActorPhotos';
@@ -11,25 +8,23 @@ import { IMovieCard } from '../../components/MovieCard/MovieCard';
 import { IPhotos } from './ActorPhotos/ActorPhotos';
 import { IActorTitleProps } from './ActorTitleInfo/ActorTitleInfo';
 
+import { useAppSelector, useAppDispatch } from '../../hooks/hooks';
+import getActorInfo from '../../store/rootStore/actorPageStore/getActorData/getActorInfo';
+import getActorImages from '../../store/rootStore/actorPageStore/getActorData/getActorPhotos';
+import getFilmsWithActor from '../../store/rootStore/actorPageStore/getActorData/getFilmsWithActor';
+
 const ActorProfile: React.FC = () => {
   const [info, setInfo] = useState<IActorTitleProps>();
   const [photos, setPhotos] = useState<Array<IPhotos>>([]);
   const [knownBy, setKnownBy] = useState<Array<IMovieCard>>([]);
   const [sortKnownBy, setSortKnownBy] = useState<Array<IMovieCard>>([]);
   const actorGridPhotosLength = 4;
+  const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    actorInfo.then((res) => {
-      setInfo((): IActorTitleProps => res);
-      return;
-    });
-    actorPhotosMock.then((res) => {
-      setPhotos((): IPhotos[] => res.profiles);
-    });
-    filmsWithActor.then((res) => {
-      setKnownBy((): any[] => res.cast);
-    });
-  });
+  const actorInfoStore = useAppSelector((state) => state.actorPageReducer);
+  const lang = useAppSelector((state) => state.mainReducer.lang);
+  const id = useAppSelector((state) => state.actorPageReducer.id);
+  const cast = useAppSelector((state) => state.actorPageReducer.cast);
 
   const sortFilmPopularity = (arr: Array<IMovieCard>) => {
     const filmsLength = 10;
@@ -39,7 +34,23 @@ const ActorProfile: React.FC = () => {
     return resultFilmsLength.slice(0, filmsLength);
   };
 
-  useMemo(() => {
+  useEffect(() => {
+    const requestInfoProperties = {
+      lang: lang,
+      id: id,
+    };
+    dispatch(getActorInfo(requestInfoProperties));
+    dispatch(getFilmsWithActor(requestInfoProperties));
+    dispatch(getActorImages(id));
+  }, [dispatch, lang, id]);
+
+  useEffect(() => {
+    setInfo(() => actorInfoStore.actorInfo);
+    setPhotos((): IPhotos[] => actorInfoStore.profiles);
+    setKnownBy((): any[] => actorInfoStore.cast);
+  }, [dispatch, lang, actorInfoStore, cast]);
+
+  useEffect(() => {
     setSortKnownBy(sortFilmPopularity(knownBy));
   }, [knownBy]);
 
@@ -49,7 +60,7 @@ const ActorProfile: React.FC = () => {
       <ActorPhotos props={photos} photosLength={actorGridPhotosLength} />
       <div className={styles.cardsWrapper}>
         {sortKnownBy.map((el: IMovieCard) => {
-          return <MovieCard key={el.id} props={el} />;
+          return <MovieCard key={Math.random() * Date.now()} props={el} />;
         })}
       </div>
     </div>
--- reactjs-films-homework/src/store/rootStore/rootStore.ts
+++ reactjs-films-homework/src/store/rootStore/rootStore.ts
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import mainSlice from './mainStore';
 import movieDetailsPageSlice from './movieDetailsPageStore';
+import actorPageSlice from './actorPageStore';
 
 const rootReducer = combineReducers({
   mainReducer: mainSlice,
   movieDetailsReducer: movieDetailsPageSlice,
+  actorPageReducer: actorPageSlice,
 });
 
 const rootStore = configureStore({
