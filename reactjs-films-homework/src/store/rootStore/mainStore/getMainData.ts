e3b4e1506c50271290cfe147b855075697b7821a
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2021-07-07 15:30:36 +0300

    fix: fixed mixed content in search URL getMainDataSearch.ts

--- reactjs-films-homework/src/store/rootStore/mainStore/getMainData.ts
+++ reactjs-films-homework/src/store/rootStore/mainStore/getMainData.ts
@@ -15,7 +15,7 @@ const getMainData = createAsyncThunk(
       const { category, lang, page, query } = props;
       const queryFromForm = query !== '' ? `&query=${query}` : '';
       const baseUrl = `https://api.themoviedb.org/3/movie/${category}?api_key=8fa5bc53bb4a09dfb6560253edf33030&language=${lang.toLowerCase()}-${lang}&page=${page}`;
-      const searchUrl = `https://api.themoviedb.org/3/search/movie/?api_key=8fa5bc53bb4a09dfb6560253edf33030&language=${lang.toLowerCase()}-${lang}&${queryFromForm}&page=${page}`;
+      const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=8fa5bc53bb4a09dfb6560253edf33030&language=${lang.toLowerCase()}-${lang}&${queryFromForm}&page=${page}`;
 
       if (query?.length) {
         try {
@@ -24,11 +24,7 @@ const getMainData = createAsyncThunk(
         } catch (error) {
           console.error('getMainData_search', error);
         }
-        console.log(searchUrl, 'searchUrl');
-        console.log(baseUrl, 'baseUrl');
       }
-      console.log(searchUrl, 'searchUrl');
-      console.log(baseUrl, 'baseUrl');
       const response = await axios.get(baseUrl);
       return response.data;
     } catch (error) {
