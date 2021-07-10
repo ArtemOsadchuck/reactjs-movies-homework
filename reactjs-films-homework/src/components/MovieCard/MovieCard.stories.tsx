3c44f709b1c4b074b6301fcdb472e8246e5afba2
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2021-07-10 21:57:56 +0300

    refactor: MovieCard.stories.tsx

--- reactjs-films-homework/src/components/MovieCard/MovieCard.stories.tsx
+++ reactjs-films-homework/src/components/MovieCard/MovieCard.stories.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import { Story } from '@storybook/react';
 import MovieCard from './MovieCard';
 
+import { Provider } from 'react-redux';
+import store from '../../store/store';
+
 export default {
   title: 'MovieCard',
   component: MovieCard,
@@ -26,28 +29,19 @@ const MovieCardMocks = {
   vote_count: 2932,
 };
 const MovieCardMocksWithoutImg = {
-  adult: false,
-  backdrop_path: '/gNBCvtYyGPbjPCuNuXR6.jpg',
-  genre_ids: [35, 18, 10749],
-  id: 19404,
-  original_language: 'hi',
-  original_title: 'दिलवाले दुल्हनिया ले जायेंगे',
-  overview:
-    'Raj is a rich, carefree, happy-go-lucky second generation NRI. Simran is the daughter of Chaudhary Baldev Singh, who in spite of being an NRI is very strict about adherence to Indian values. Simran has left for India to be married to her childhood fiancé. Raj leaves for India with a mission at his hands, to claim his lady love under the noses of her whole family. Thus begins a saga.',
-  popularity: 16.457,
+  ...MovieCardMocks,
   poster_path: '',
-  release_date: '1995-10-20',
-  title: 'Dilwale Dulhania Le Jayenge',
-  video: false,
-  vote_average: 8.7,
-  vote_count: 2932,
 };
 
 const TemplateUrl: Story<typeof MovieCard> = () => (
-  <MovieCard props={MovieCardMocks} />
+  <Provider store={store}>
+    <MovieCard props={MovieCardMocks} />
+  </Provider>
 );
 const Template: Story<typeof MovieCard> = () => (
-  <MovieCard props={MovieCardMocksWithoutImg} />
+  <Provider store={store}>
+    <MovieCard props={MovieCardMocksWithoutImg} />
+  </Provider>
 );
 
 export const MovieCardImg = TemplateUrl.bind({});
