cb3fc0163a847ef6b7a7159e70ff2fa217d5cd00
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2021-07-10 15:41:54 +0300

    fix: Headet tests
    update snapsot

--- reactjs-films-homework/src/components/Header/__tests__/Header.spec.tsx
+++ reactjs-films-homework/src/components/Header/__tests__/Header.spec.tsx
@@ -6,29 +6,33 @@ import store from '../../../store/store';
 
 describe('Header', () => {
   let Fragment: any;
+
   beforeEach(() => {
-    const asFragment = render(
+    const { asFragment } = render(
       <Provider store={store}>
         <Header />
       </Provider>
     );
-    Fragment = asFragment;
+    Fragment = asFragment();
   });
 
   afterEach(() => {
     cleanup();
   });
 
-  it('Header snapshot', () => {
+  it('Snapshot', () => {
     expect(Fragment).toMatchSnapshot();
   });
-  it('Header Must include img', () => {
+
+  it('Must include img', () => {
     expect(screen.getByRole('img')).toBeInTheDocument();
   });
-  it('Header Must include h1', () => {
+
+  it('Must include h1', () => {
     expect(screen.getByRole('heading')).toBeInTheDocument();
   });
-  it('Header Must include button', () => {
+
+  it('Must include button', () => {
     expect(screen.getByRole('button')).toBeInTheDocument();
   });
 });
