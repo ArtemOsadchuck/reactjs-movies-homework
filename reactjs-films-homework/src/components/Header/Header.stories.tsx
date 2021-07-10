c57935bde7c9514c0682adf96b363ffb770d2b29
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2021-07-10 15:49:37 +0300

    refactor: Header.stories.tsx
    moved 'export default' to the end of file

--- reactjs-films-homework/src/components/Header/Header.stories.tsx
+++ reactjs-films-homework/src/components/Header/Header.stories.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-
 import { Story } from '@storybook/react';
 import Header from './Header';
-
-export default {
-  title: 'Header',
-  component: Header,
-};
 
 const Template: Story<typeof Header> = () => <Header />;
 
 export const HeaderStory = Template.bind({});
+export default {
+  title: 'Header',
+  component: Header,
+};
