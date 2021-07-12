99ab49f0bf066b32a2c3920470877abf6e458de1
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2021-07-12 13:26:33 +0300

    refactor: moved default export to the end of file

--- reactjs-films-homework/src/pages/App/App.stories.tsx
+++ reactjs-films-homework/src/pages/App/App.stories.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 import { Story } from '@storybook/react';
 import App from './App';
 
+const Template: Story<typeof App> = () => <App />;
+
+export const FirstStory = Template.bind({});
+
 export default {
   title: 'App',
   component: App,
 };
-
-const Template: Story<typeof App> = () => <App />;
-
-export const FirstStory = Template.bind({});
