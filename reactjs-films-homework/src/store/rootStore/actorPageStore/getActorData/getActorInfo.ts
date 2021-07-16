dec53d745e16cf276341f8cde1c7098133d45d85
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2021-07-16 11:22:43 +0300

    feat: added check

--- reactjs-films-homework/src/store/rootStore/actorPageStore/getActorData/getActorInfo.ts
+++ reactjs-films-homework/src/store/rootStore/actorPageStore/getActorData/getActorInfo.ts
@@ -11,9 +11,11 @@ const getActorInfo = createAsyncThunk(
   async (props: IGetActorInfo) => {
     try {
       const { lang, id } = props;
-      const baseUrl = `https://api.themoviedb.org/3/person/${id}?api_key=8fa5bc53bb4a09dfb6560253edf33030&language=${lang.toLowerCase()}-${lang}`;
-      const response = await axios.get(baseUrl);
-      return response.data;
+      if (typeof id === 'string' && lang) {
+        const baseUrl = `https://api.themoviedb.org/3/person/${id}?api_key=8fa5bc53bb4a09dfb6560253edf33030&language=${lang.toLowerCase()}-${lang}`;
+        const response = await axios.get(baseUrl);
+        return response.data;
+      }
     } catch (error) {
       console.error('getActorInfo', error);
     }
