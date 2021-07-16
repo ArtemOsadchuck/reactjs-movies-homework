cfc289dbb3b00a5ce3d2059c581e2f2af141a3b5
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2021-07-16 09:34:51 +0300

    feat: update Form.tsx, to work with the router
    removed redundant request, imports, and variables

--- reactjs-films-homework/src/components/Header/Form/Form.tsx
+++ reactjs-films-homework/src/components/Header/Form/Form.tsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import search from './assets/search.png';
 import styles from './Form.module.scss';
-import { useAppSelector, useAppDispatch } from '../../../hooks/hooks';
+import { useAppDispatch } from '../../../hooks/hooks';
 import {
   setQuery,
   setActivePage,
 } from '../../../store/rootStore/mainStore/mainSlice';
-import getMainData from '../../../store/rootStore/mainStore/getMaiData/getMainData';
+
+import { Link, useHistory } from 'react-router-dom';
+import useUrlSearch from '../../../hooks/useUrlSearch';
 
 type IFormProps = {
   placeholder: string;
@@ -14,20 +16,23 @@ type IFormProps = {
 
 const Form: React.FC<IFormProps> = ({ placeholder }) => {
   const [inputValue, setInputValue] = useState<string>('');
+  const [inputSearchValue, setInputSearchValue] = useState<string>('');
+
   const pageAfterSearch = '1';
+
   const dispatch = useAppDispatch();
+  const history = useHistory();
+  const activeUrlCategory = useUrlSearch('category');
+  const activeUrlPage = useUrlSearch('page');
+  const activeUrlSearch = useUrlSearch('search');
+
   const [placeholderState, setPlaceholderState] = useState<string>(placeholder);
-  const mainState = {
-    lang: useAppSelector((state) => state.mainReducer.lang),
-    category: useAppSelector((state) => state.mainReducer.category),
-    page: pageAfterSearch,
-  };
 
   const resetInput = () => {
     const checkIsOnBlur = document.body.addEventListener(
       'click',
       (event: any) => {
-        const id = event.target.id;
+        const id: string = event.target.id;
         if (id === 'search') {
           setInputValue('');
         }
@@ -41,13 +46,31 @@ const Form: React.FC<IFormProps> = ({ placeholder }) => {
     return checkIsOnBlur;
   };
 
+  useEffect(() => {
+    setPlaceholderState((prev) => {
+      return (prev = !activeUrlSearch
+        ? placeholder
+        : `Results for ${activeUrlSearch}`);
+    });
+
+    activeUrlPage && dispatch(setActivePage(pageAfterSearch));
+    activeUrlSearch && dispatch(setQuery(activeUrlSearch));
+  }, [activeUrlPage, activeUrlSearch, dispatch, placeholder]);
+
+  useEffect(() => {
+    activeUrlCategory && setPlaceholderState((prev) => placeholder);
+    setInputSearchValue('');
+  }, [placeholder, activeUrlCategory]);
+
   const getSearchResults = () => {
     setPlaceholderState((prev) => {
       return (prev = !inputValue ? placeholder : `Results for ${inputValue}`);
     });
+
     dispatch(setActivePage(pageAfterSearch));
     dispatch(setQuery(inputValue));
-    dispatch(getMainData({ ...mainState, query: inputValue }));
+    setInputValue('');
+    setInputSearchValue('');
   };
 
   return (
@@ -61,13 +84,15 @@ const Form: React.FC<IFormProps> = ({ placeholder }) => {
           autoComplete="off"
           onChange={(event) => {
             setInputValue((prev) => (prev = event.target.value));
+            setInputSearchValue((prev) => (prev = event.target.value));
           }}
           onKeyPress={(event) => {
             if (event.code === 'Enter') {
               event.preventDefault();
               getSearchResults();
-              setInputValue('');
-              event.preventDefault();
+              history.push(
+                `/?search=${inputSearchValue}&page=${pageAfterSearch}`
+              );
             }
           }}
           value={inputValue}
@@ -80,7 +105,9 @@ const Form: React.FC<IFormProps> = ({ placeholder }) => {
             getSearchResults();
           }}
         >
-          <img src={search} id={'search'} alt="" width="16px" />
+          <Link to={`/?search=${inputSearchValue}&page=${pageAfterSearch}`}>
+            <img src={search} id={'search'} alt="" width="16px" />
+          </Link>
         </div>
       </form>
     </div>
