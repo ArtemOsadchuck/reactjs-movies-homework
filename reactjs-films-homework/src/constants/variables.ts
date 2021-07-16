196bb83fec7b82dd6a1b69b426e1e32600762808
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2021-07-16 18:08:31 +0300

    feat: added constants of the application

---
+++ reactjs-films-homework/src/constants/variables.ts
@@ -0,0 +1,7 @@
+export const initialLanguage = 'EN';
+export const neededMainPages = 5;
+export const startAppPage = '1';
+export const recommendationsMovieDetailsQuality = 5;
+export const movieDetailsImagesBlockQuality = 8;
+export const movieDetailsImagesWidth = '172px';
+export const topBilletCastImagesHight = '160px';
