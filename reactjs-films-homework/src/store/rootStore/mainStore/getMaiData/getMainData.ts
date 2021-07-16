524e90d4be4cab08f1cbd6780642ef0f38ca0ff7
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2021-07-16 11:24:50 +0300

    feat: added check to suppress unnecessary requests

--- reactjs-films-homework/src/store/rootStore/mainStore/getMaiData/getMainData.ts
+++ reactjs-films-homework/src/store/rootStore/mainStore/getMaiData/getMainData.ts
@@ -4,8 +4,8 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 export interface IGetMainData {
   category: string;
   lang: string;
-  page: string;
-  query?: string;
+  page: string | undefined;
+  query?: string | null;
 }
 
 const getMainData = createAsyncThunk(
@@ -17,7 +17,9 @@ const getMainData = createAsyncThunk(
       const baseUrl = `https://api.themoviedb.org/3/movie/${category}?api_key=8fa5bc53bb4a09dfb6560253edf33030&language=${lang.toLowerCase()}-${lang}&page=${page}`;
       const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=8fa5bc53bb4a09dfb6560253edf33030&language=${lang.toLowerCase()}-${lang}&${queryFromForm}&page=${page}`;
 
-      if (query?.length) {
+      // console.log(searchUrl);
+      if (query?.length && page?.length && lang) {
+        console.log(searchUrl);
         try {
           const response = await axios.get(searchUrl);
           return response.data;
@@ -25,9 +27,10 @@ const getMainData = createAsyncThunk(
           console.error('getMainData_search', error);
         }
       }
-
-      const response = await axios.get(baseUrl);
-      return response.data;
+      if (!query?.length) {
+        const response = await axios.get(baseUrl);
+        return response.data;
+      }
     } catch (error) {
       console.error('getMainData', error);
     }
