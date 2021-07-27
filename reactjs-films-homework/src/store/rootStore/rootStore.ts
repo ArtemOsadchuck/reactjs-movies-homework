5b2424d7b9758ba7157ab5931ec3d8b6bfb046b1
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2021-07-27 16:42:25 +0300

    refactor: removed redundant comments

--- reactjs-films-homework/src/store/rootStore/rootStore.ts
+++ reactjs-films-homework/src/store/rootStore/rootStore.ts
@@ -14,4 +14,3 @@ const rootStore = configureStore({
 });
 
 export default rootStore;
-//
--- reactjs-films-homework/src/store/store.ts
+++ reactjs-films-homework/src/store/store.ts
@@ -3,4 +3,3 @@ import rootStore from './rootStore/rootStore';
 export type RootState = ReturnType<typeof rootStore.getState>;
 export type AppDispatch = typeof rootStore.dispatch;
 export default rootStore;
-//
