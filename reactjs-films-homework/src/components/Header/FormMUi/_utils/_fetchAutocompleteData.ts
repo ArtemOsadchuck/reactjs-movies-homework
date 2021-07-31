0b32333e36e20ac75291e28a4fabfa30f5053587
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2021-07-31 19:23:50 +0300

    feat: add "try catch", and lang to the _fetchAutocompleteData.ts

--- reactjs-films-homework/src/components/Header/FormMUi/_utils/_fetchAutocompleteData.ts
+++ reactjs-films-homework/src/components/Header/FormMUi/_utils/_fetchAutocompleteData.ts
@@ -1,11 +1,15 @@
-const fetchAutoCompleteData = async (movieSearch: string) => {
-  if (movieSearch.length >= 1) {
-    const queryFromForm = movieSearch !== '' ? `&query=${movieSearch}` : '';
-    const response = await fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=8fa5bc53bb4a09dfb6560253edf33030&language=en-EN&${queryFromForm}&page=1`
-    );
-    const movies = await response.json();
-    return movies.results;
+const fetchAutoCompleteData = async (movieSearch: string, lang: string) => {
+  if (movieSearch.length >= 1 && lang) {
+    try {
+      const queryFromForm = movieSearch !== '' ? `&query=${movieSearch}` : '';
+      const response = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=8fa5bc53bb4a09dfb6560253edf33030&language=${lang.toLowerCase()}-${lang}&${queryFromForm}&page=1`
+      );
+      const movies = await response.json();
+      return movies.results;
+    } catch (error) {
+      console.error('fetchAutoCompleteData', error);
+    }
   }
 };
 
