d72ca4fa5c0ac68fede2498bfdd1e21ac0c44eee
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2021-07-31 19:21:30 +0300

    feat: create a schema to validate search form

---
+++ reactjs-films-homework/src/components/Header/FormMUi/_utils/formValidation.ts
@@ -0,0 +1,12 @@
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+  movie: yup
+    .string()
+    .max(35)
+    .min(1)
+    .matches(/[a-zа-я0-9]/i)
+    .required(),
+});
+
+export default schema;
